2fb491a7ec0d8f728f4ac95e9eb6f1b4901f028b
gabrielld06 <gabriellimadias2001@gmail.com>
2020-11-19 21:12:57 -0300

    Algumas coisas ai

--- ArvoreB.c
+++ ArvoreB.c
@@ -3,7 +3,10 @@
 #include <string.h>
 
 #define M 5
-#define NULO -1;
+#define NULO -1
+#define PROMOCAO 5 //ajustar valor
+#define SEM_PROMOCAO 2
+#define INSERCAO 3
 
 typedef struct cabecalho_raiz {
 	int raiz;
@@ -27,6 +30,13 @@ void escreve_pagina(int rrn, pagina *pg, FILE *arvb) {
 	fwrite(pg, sizeof(pagina), 1, arvb);
 }
 
+int novo_rrn(FILE *arvb) {
+	int offset;
+	fseek(arvb, 0, SEEK_END);
+	offset = ftell(arvb);
+	return (offset-sizeof(cabecalho))/sizeof(pagina);
+}
+
 int inicializa_pagina(pagina *pg) {
 	int i;
 	for(i = 0;i < M;i++) {
@@ -37,19 +47,125 @@ int inicializa_pagina(pagina *pg) {
 	pg -> num_chaves = 0;
 }
 
-void insere_na_pagina(int chave, int filhos[], pagina *pg) {
-	
+void insere_na_pagina(int chave, int filhoD, pagina *pg) {
+	int i;
+	for(i = pg->num_chaves;i > 0 && chave < pg->chaves[i-1];i--) {
+		pg->chaves[i] = pg->chaves[i-1];
+		pg->filhos[i+1] = pg->filhos[i];
+	}
+	pg->num_chaves++;
+	pg->chaves[i] = chave;
+	pg->filhos[i+1] = filhoD;
 }
 
-int le_pagina(pagina *pg) {
-	int i;
-	for(i = 0;i < M;i++) {
-		pg -> chaves[i] = NULO;
-		pg -> filhos[i] = NULO;
+void divide(int chave, int rrn, pagina *pg, int *chave_pro, int *filho_d_pro, pagina *novapagina, FILE *arvb) {
+	int pagaux_chaves[M], pagaux_filhos[M+1], pagaux_num_chaves;
+	int i, med;
+	
+	for(i = 0;i < pg->num_chaves;i++) {
+		pagaux_chaves[i] = pg->chaves[i];
+		pagaux_filhos[i] = pg->filhos[i];
 	}
-	pg -> filhos[i] = NULO;
-	pg -> num_chaves = 0;
+	pagaux_filhos[i] = pg->filhos[i];
+	pagaux_num_chaves = pg->num_chaves;
+	
+	insere_na_pagina(chave, rrn, pg);
+	
+	med = pagaux_num_chaves/2;
+	*chave_pro = pagaux_chaves[med];
+	
+	inicializa_pagina(pg);
+	for(i = 0;i < med;i++) {
+		pg->chaves[i] = pagaux_chaves[i];
+		pg->filhos[i] = pagaux_filhos[i];
+		pg->num_chaves++;
+	}
+	pg->filhos[i] = pagaux_filhos[i];
+	
+	inicializa_pagina(novapagina);
+	for(i = med+1;i < pagaux_num_chaves;i++) {
+		novapagina->chaves[novapagina->num_chaves] = pagaux_chaves[i];
+		novapagina->filhos[novapagina->num_chaves] = pagaux_filhos[i];
+		novapagina->num_chaves++;
+	}
+	novapagina->filhos[novapagina->num_chaves] = pagaux_filhos[i];
+	
+	*filho_d_pro = novo_rrn(arvb);
 }
+
+int inserir(int rrn_atual, int chave, int *chave_pro, int *filho_d_pro, FILE *arvb) {
+	pagina pg, novapg;
+	int chv_pro, rrn_pro;
+	int pos;
+	int retorno;
+	
+	if(rrn_atual == NULO) {
+		*chave_pro = chave;
+		*filho_d_pro = NULO;
+		return PROMOCAO;
+	}
+	
+	le_pagina(rrn_atual, &pg, arvb);
+	
+	//pos = pos_chave(chave, pg->chave, pg->num_chaves); TODO
+	if(pos < pg.num_chaves && pg.chaves[pos] == chave) return -1;
+	
+	retorno = inserir(pg.filhos[pos], chave, &chv_pro, &rrn_pro, arvb);
+	
+	if(retorno == SEM_PROMOCAO || retorno == -1)
+		return retorno;
+	else {
+		if(pg.num_chaves < M-1) {
+			insere_na_pagina(chv_pro, rrn_pro, &pg);
+			escreve_pagina(rrn_atual, &pg, arvb);
+			return SEM_PROMOCAO;
+		} else {
+			divide(chv_pro, rrn_pro, &pg, chave_pro, filho_d_pro, &novapg, arvb);
+			escreve_pagina(rrn_atual, &pg, arvb);
+			escreve_pagina(*filho_d_pro, &novapg, arvb);
+			return PROMOCAO;
+		}
+	}
+}
+
+int inserir_chave(int chave, int *rrn_raiz, FILE *arvb) {
+	pagina raiz;
+	int chave_pro, filho_d_pro;
+	int retorno;
+	
+	retorno = inserir(*rrn_raiz, chave, &chave_pro, &filho_d_pro, arvb);
+	
+	if(retorno == -1)
+		return -1;
+	else if(retorno == PROMOCAO) {
+		pagina novaraiz;
+		
+		inicializa_pagina(&novaraiz);
+		novaraiz.chaves[0] = chave_pro;
+		novaraiz.filhos[0] = *rrn_raiz;
+		novaraiz.filhos[1] = filho_d_pro;
+		novaraiz.num_chaves = 1;
+		
+		*rrn_raiz = novo_rrn(arvb);
+		escreve_pagina(*rrn_raiz, &novaraiz, arvb);
+	}
+	return INSERCAO;
+}
+
+int ler_chave(FILE *file, int *chave) {
+	return fscanf(file, "%d", chave);
+}
+
+/* void criar(char *nome_arq) {
+	FILE *chaves, *arvb;
+	int reg_cont, i;
+	int chave;
+	char buffer[500];
+	cabecalho cab;
+	pagina raiz;
+	
+	
+} */
 
 int main(int argc, char **argv) {
 
